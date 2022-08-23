bcbce76dc3ec0cf3147c03db6425e96237775327
TAEHYEOK LEE <luketae0220@gmail.com>
2022-08-22 22:20:08 -0700

    page 92 greedy2

--- greedy/greedy_03.py
+++ greedy/greedy_03.py
@@ -2,6 +2,29 @@ import time
 start_time = time.time()
 
 # Program source code
+# input data with split space n, m, k
+n,m,k = map(int, input().split())
+
+data = list(map(int, input().split()))
+
+data.sort()
+first = data[n-1]
+second = data[n-2]
+
+result = 0
+
+while True:
+    for i in range(k):
+        if m == 0:
+            break
+        result += first
+        m -= 1
+    if m == 0:
+        break
+    result += second
+    m -= 1
+
+print(result)
 
 # Time check
 end_time = time.time()
